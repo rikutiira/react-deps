55403b83f5e540acf37bc33c98a07e2c75869d61
Riku Tiira <riku@tiira.net>
2016-08-11 17:00:46 +0300

    Refactored code so that it works in multi-user node server environment

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import ReactDOMServer from 'react-dom/server'
 
-let usedDependencies = new Map()
-let initialRender = false
 let isClient = typeof window !== 'undefined' && window.document
 
 const flatten = (arr = []) => !arr.length ? arr : arr.reduce((flattenedArr, item) => flattenedArr.concat(item))
@@ -36,8 +34,33 @@ const promiseUtils = {
     })
 }
 
-const renderUntilReady = (element) => {
-    const html = ReactDOMServer.renderToStaticMarkup(element)
+const Container = React.createClass({
+    childContextTypes: {
+        __reactDeps: React.PropTypes.object
+    },
+
+    getInitialState() {
+        return {
+            initialRender: true
+        }
+    },
+
+    getChildContext() {
+        return {
+            __reactDeps: {
+                usedDependencies: this.props.usedDependencies,
+                initialRender: this.state.initialRender
+            }
+        }
+    },
+
+    render() {
+        return <div>{this.props.children}</div>
+    }
+})
+
+const renderUntilReady = (element, usedDependencies) => {
+    const html = ReactDOMServer.renderToStaticMarkup(<Container usedDependencies={usedDependencies} initialRender>{element}</Container>)
 
     return promiseUtils.all(
         Array.from(usedDependencies.keys()).map((key) => {
@@ -50,67 +73,65 @@ const renderUntilReady = (element) => {
                         resolvedDeps: zipObj(depKeys, resolved)
                     }))
 
-                    return renderUntilReady(element)
+                    return renderUntilReady(element, usedDependencies)
                 })
         })
     ).then((htmlArr) => htmlArr[htmlArr.length-1])
 }
 
-export const load = (dependencies = {}, waitForAll) => (Component) => {
-    return React.createClass({
-        componentWillMount() {
-            waitForAll = initialRender ? true : waitForAll
+export const load = (dependencies = {}, waitForAll) => (Component) => React.createClass({
+    contextTypes: {
+        __reactDeps: React.PropTypes.object
+    },
 
-            const usedDeps = usedDependencies.has(dependencies)
-            const depKeys = Object.keys(dependencies)
+    componentWillMount() {
+        const { usedDependencies, initialRender } = this.context.__reactDeps
+        const usedDeps = usedDependencies.has(dependencies)
+        const depKeys = Object.keys(dependencies)
 
-            if (usedDependencies.has(dependencies) && initialRender) {
-                this.setState(usedDependencies.get(dependencies).resolvedDeps)
+        if (usedDependencies.has(dependencies) && initialRender) {
+            this.setState(usedDependencies.get(dependencies).resolvedDeps)
+        } else {
+            const deps = depKeys.map((key) => Promise.resolve(dependencies[key](this.props, initialRender)))
+            usedDependencies.set(dependencies, { deps, depKeys })
+
+            if (initialRender || waitForAll) {
+                promiseUtils.all(deps).then((resolvedDeps) => {
+                    if (isClient) {
+                        this.setState(zipObj(depKeys, resolvedDeps))
+                    }
+                })
             } else {
-                const deps = depKeys.map((key) => Promise.resolve(dependencies[key](this.props, initialRender)))
-                usedDependencies.set(dependencies, { deps, depKeys })
-
-                if (waitForAll) {
-                    promiseUtils.all(deps).then((resolvedDeps) => {
+                deps.forEach((dep, idx) => {
+                    dep.then((resolvedDep) => {
                         if (isClient) {
-                            this.setState(zipObj(depKeys, resolvedDeps))
+                            this.setState({ [depKeys[idx]]: resolvedDep })
                         }
                     })
-                } else {
-                    deps.forEach((dep, idx) => {
-                        dep.then((resolvedDep) => {
-                            if (isClient) {
-                                this.setState({ [depKeys[idx]]: resolvedDep })
-                            }
-                        })
-                    })
-                }
+                })
             }
-        },
-
-        render() {
-            return <Component {...this.props} {...this.state} />
         }
-    })
-}
+    },
+
+    render() {
+        return <Component {...this.props} {...this.state} />
+    }
+})
 
 export const render = (element, node) => {
-    usedDependencies = new Map()
-    initialRender = true
-    ReactDOM.render(element, node)
+    let usedDependencies = new Map()
+    let disableInitialRender = {}
+    let container
+
+    ReactDOM.render(<Container usedDependencies={usedDependencies} ref={(ref) => container = ref}>{element}</Container>, node)
 
     promiseUtils.all(flatten(pick(Array.from(usedDependencies.values()), 'deps'))).then(() => {
-        initialRender = false
+        container.setState({ initialRender: false })
     })
 }
 
 export const renderToString = (element) => {
-    usedDependencies = new Map()
-    initialRender = true
+    let usedDependencies = new Map()
 
-    return renderUntilReady(element).then((html) => {
-        initialRender = false
-
-        return html
-    })
+    return renderUntilReady(element, usedDependencies).then((html) => html)
 }
